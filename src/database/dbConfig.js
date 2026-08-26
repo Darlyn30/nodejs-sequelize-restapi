a90e093bed178b613205a2500792fe3d7a4afc4a
Darlyn Olivo <105079724+Darlyn30@users.noreply.github.com>
2026-08-25 22:30:17 -0400

    Correct Sequelize configuration in dbConfig.js
    Fix Sequelize initialization by adding missing comma and correcting comments.

--- src/database/dbConfig.js
+++ src/database/dbConfig.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 //process.loadEnvFile(); //para poder traer las variables de entorno de mi .env
 
-export const sequelize = new Sequelize(process.env.DB_URL { //db, user, pass -> vendran del .env
-  //host: 'localhost'
-  dialect: 'mysql'
+// En Railway ya tienes la variable MYSQL_URL lista
+export const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: "mysql"
 });
