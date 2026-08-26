c34fb93e2d7d0d6347548755f32945f753e5a6f7
Darlyn Olivo <105079724+Darlyn30@users.noreply.github.com>
2026-08-25 22:21:42 -0400

    Update dbConfig.js

--- src/database/dbConfig.js
+++ src/database/dbConfig.js
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-process.loadEnvFile(); //para poder traer las variables de entorno de mi .env
+//process.loadEnvFile(); //para poder traer las variables de entorno de mi .env
 
 export const sequelize = new Sequelize('pruebasequelize', process.env.DB_USER, process.env.DB_PASS, { //db, user, pass -> vendran del .env
   host: 'localhost',
