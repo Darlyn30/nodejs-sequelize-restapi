773bc0d5dfcdb2bc998ac247169cbd022e6bf200
Darlyn30 <zomberkawaiimusic@gmail.com>
2026-08-26 21:01:51 +0200

    fix: correcion apuntando a app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import projectRoutes from "./routes/project.routes.js";
 import taskRoutes from "./routes/task.routes.js";
+import { sequelize } from "./database/dbConfig.js";
+
 
 const app = express();
 
@@ -10,4 +12,18 @@ app.use(express.json());
 app.use("/projects", projectRoutes);
 app.use("/tasks", taskRoutes);
 
-export default app;
+
+
+
+
+app.listen(3001, async() => {
+      try {
+        await sequelize.sync({force: false});
+        app.listen(3000);
+        console.log("Server on port 3000");
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+});
+
+// export default app;
--- src/database/dbConfig.js
+++ src/database/dbConfig.js
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
-//process.loadEnvFile(); //para poder traer las variables de entorno de mi .env
+process.loadEnvFile(); //para poder traer las variables de entorno de mi .env
 
 // En Railway ya tienes la variable MYSQL_URL lista
-export const sequelize = new Sequelize(process.env.DB_URL, {
-  dialect: "mysql"
+export const sequelize = new Sequelize("pruebasequelize", process.env.DB_USER, process.env.DB_PASSWORD, {
+  dialect: "mysql",
+  host: process.env.DB_HOST
 });
