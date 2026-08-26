350f376ceb408a1d8cd8d2eefc657cdf6d97c795
Darlyn Olivo <105079724+Darlyn30@users.noreply.github.com>
2026-08-25 23:18:20 -0400

    Update project.routes.js

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -2,13 +2,12 @@ import { Router } from "express";
 const router = Router();
 import { getProjects, getProjectTasks, createProject, updateProject, deleteProject, getProjectById} from "../controllers/project.controller.js";
 
-router.get("/", getProjects);
-
-router.get("/:id", getProjectById);
 router.post("/create", createProject);
 router.put("/update/:id", updateProject);
 router.delete("/delete/:id", deleteProject);
-
 router.get("/:id/tasks", getProjectTasks);
+router.get("/:id", getProjectById);
+router.get("/", getProjects);
+
 
 export default router;
